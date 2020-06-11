27fb0e3f95259e2141b89284e3f113d444eb0d9c
Magno Dantas <magnodantas1991@gmail.com>
2020-06-11 13:02:34 -0300

    Desafio de upload finalizado

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import crypto from 'crypto';
+import multer from 'multer';
+import path from 'path';
+
+const tmpfolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpfolder,
+  storage: multer.diskStorage({
+    destination: tmpfolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const filename = `${fileHash}-${file.originalname}`;
+
+      return callback(null, filename);
+    },
+  }),
+};
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,85 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface CSVTransactions {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const contactReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const parseCsv = contactReadStream.pipe(parsers);
+
+    const transactions: CSVTransactions[] = [];
+    const categories: string[] = [];
+
+    parseCsv.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+    await new Promise(resolve => parseCsv.on('end', resolve));
+
+    const existentCategories = await categoryRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existentCategoriesTitle = existentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategoryTitles = categories
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoryRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoryRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existentCategories];
+
+    const createdTransactions = transactionRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionRepository.save(createdTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions;
   }
 }
 
